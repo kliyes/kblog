f0339b0d2c3bfed8372baaedf73fbc2abf8ef03c
tom.kliyes <jingyang.tom@qq.com>
2014-01-15 23:53:13 +0800

    --

--- src/comment/admin.py
+++ src/comment/admin.py
@@ -4,3 +4,12 @@
 # Created on 2013-11-15, by Tom
 #
 #
+from django.contrib import admin
+from comment.models import Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['blog', 'contact', 'name']
+    list_filter = ['blog']
+
+admin.site.register(Comment, CommentAdmin)
