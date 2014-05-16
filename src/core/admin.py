fc6dbed860086e01fabe431ab9added2ac88a215
yang.jing <yang.jing@lianbi.com.cn>
2014-05-16 18:17:24 +0800

    修改：第二套模板样式表

--- src/core/admin.py
+++ src/core/admin.py
@@ -4,13 +4,21 @@
 # Created on May 8, 2014, by tom
 #
 #
-from django.contrib import admin
-from core.models import Themes
 __author__ = 'tom'
+
+from django.contrib import admin
+
+from core.models import Themes
+
+
+def preview(obj):
+    preview.allow_tags = True
+    return "<a href='%s' target=_blank><img src=%s width=300 height=119 />" % (obj.preview.url, obj.preview.url)
+preview.short_description = u'预览图'
 
 
 class ThemesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'preview', 'is_valid')
+    list_display = ('name', preview, 'is_valid')
     list_editable = ('is_valid',)
 
 
