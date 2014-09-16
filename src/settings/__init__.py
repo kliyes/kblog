2293962265e77f65fe2a1fcd7a519de6fdd5ce31
yang.jing <jingyang.tom@qq.com>
2014-09-16 09:25:59 +0800

    增加百度分享组件

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -3,7 +3,7 @@ from local import *
 try:
     from django.contrib.auth.models import User
     from core.models import Themes
-    
+
     themes = Themes.objects.filter(is_valid=True)
     THEME_DIRS = 'themes/templates_%s' % themes[0].templates_id if themes else '1'
     TEMPLATE_DIRS = os.path.join(os.path.dirname(__file__), '../', THEME_DIRS).replace('\\','/'),
