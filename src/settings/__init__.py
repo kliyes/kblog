089ca5f70405463ad502570305fb8dae00dc77c8
jingyang <tom@kliyes.com>
2015-08-04 09:38:20 +0800

    1.bug fixed; 2.remove local files.

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -5,7 +5,7 @@ try:
     from core.models import Themes
 
     themes = Themes.objects.filter(is_valid=True)
-    THEME_DIRS = 'themes/templates_%s' % themes[0].templates_id if themes else '1'
+    THEME_DIRS = 'themes/templates_%s' % (themes[0].templates_id if themes else '4')
     TEMPLATE_DIRS = os.path.join(os.path.dirname(__file__), '../', THEME_DIRS).replace('\\','/'),
     User._meta.verbose_name_plural = u'用户'
     User._meta.verbose_name = u'用户'
